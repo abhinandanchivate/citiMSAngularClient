06162ff8d9063801c189566f33bfba56659444f1
abhinandanchivate <abhinandanchivate2000@gmail.com>
2023-06-05 09:53:58 +0530

    updated one

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/layouts/header/header.component';
 import { LandingComponent } from './components/layouts/landing/landing.component';
 import { FooterComponent } from './components/layouts/footer/footer.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [HeaderComponent, LandingComponent, FooterComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   exports: [HeaderComponent, LandingComponent, FooterComponent],
 }) // 3 components will be exported i.e. we are granting the permission to access them outside of core module.
 export class CoreModule {}
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { UserRoutingModule } from './user-routing.module';
+import { RegisterComponent } from './components/auth/register/register.component';
+import { LoginComponent } from './components/auth/login/login.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    RegisterComponent,
+    LoginComponent
+  ],
   imports: [
     CommonModule,
     UserRoutingModule
